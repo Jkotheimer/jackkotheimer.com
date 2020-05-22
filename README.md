650a205071cb73e8c8e9a2e3c9f1f00bd77134c4
root <root@ip-172-31-81-12.ec2.internal>
2020-05-22 03:44:23 +0000

    Initial commit on new server

---
+++ README.md
@@ -0,0 +1,1 @@
+# Jackkotheimer.com
---
+++ www/site.js
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+function open_navbar() {
+	
+}
+
+function toggle_view(element) {
+	if(element.style.display == "block") 
+		element.style.display = "none";
+	else element.style.display = "block";
+}
