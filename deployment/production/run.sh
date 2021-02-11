4d35e1bdf98a3b90f5fc29867f3dedea366ee721
Jack Kotheimer <jkotheimer9@gmail.com>
2021-02-11 04:53:06 -0600

    Added ssl support in nginx.conf and hardened the server. Accounted for those changes in the production run script, and fixed the volume mount point in the docker-compose.yml

--- deployment/production/run.sh
+++ deployment/production/run.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-[ -z "$ENV" ] && ENV=dev
-cp /srv/deployment/$ENV/nginx.conf /etc/nginx/nginx.conf
+cp /srv/deployment/production/nginx.conf /etc/nginx/nginx.conf
 service nginx status &>/dev/null && service nginx restart || service nginx start
 
 # Keep the container alive forever, overwriting old versions as new ones present themselves
 while :; do
-	diff /srv/deployment/$ENV/run.sh $0 &>/dev/null
-	[ $? -eq 1 ] && cp /srv/deployment/$ENV/run.sh $0 && exec $0 $@
+	diff /srv/deployment/production/run.sh $0 &>/dev/null
+	[ $? -eq 1 ] && cp /srv/deployment/production/run.sh $0 && exec $0 $@
 	
 	sleep 30
 done
