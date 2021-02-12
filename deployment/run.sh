d639c52d422788ed4d56ce74441d49a3a369c9da
jkotheimer <jkotheimer@luc.edu>
2021-02-12 00:43:16 -0600

    Moved to one unanimous deployment script and updated production config to be more stable

--- deployment/run.sh
+++ deployment/run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-cp /srv/deployment/dev/nginx.conf /etc/nginx/nginx.conf
+[ -z "$ENV" ] && ENV=dev
+
+cp /srv/deployment/$ENV/nginx.conf /etc/nginx/nginx.conf
 service nginx status &>/dev/null && service nginx restart || service nginx start
 
 # Keep the container alive forever, overwriting old versions as new ones present themselves
 while :; do
-	diff /srv/deployment/dev/run.sh $0 &>/dev/null
-	[ $? -eq 1 ] && cp /srv/deployment/dev/run.sh $0 && exec $0 $@
+	diff /srv/deployment/$ENV/run.sh $0 &>/dev/null
+	[ $? -eq 1 ] && cp /srv/deployment/$ENV/run.sh $0 && exec $0 $@
 	
 	sleep 30
 done
